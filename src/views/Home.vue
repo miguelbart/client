2d581a434937f5423a4a89a3d4ed1eb97ee50b38
miguelbart <miguelbarts@gmail.com>
2021-10-07 00:38:46 -0500

    adding names

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,31 +3,31 @@
     <h2>Este es el Home</h2>
     <div class="ui horizontal list">
   <div class="item">
-    <img class="ui mini circular image" src="../assets/elyse.png">
+    <img class="ui mini circular image" src="../assets/jhon.png">
     <div class="content">
-      <div class="ui sub header">Molly</div>
+      <div class="ui sub header">Jhon</div>
       Coordinador-Jhon Orozco 
     </div>
   </div>
 <div class="ui horizontal list">
   <div class="item">
-    <img class="ui mini circular image" src="../assets/elyse.png">
+    <img class="ui mini circular image" src="../assets/yohan.png">
     <div class="content">
-      <div class="ui sub header">Connor</div>
+      <div class="ui sub header">Yohan</div>
       Developer Yohan
     </div>
   </div>
   <div class="item">
-    <img class="ui mini circular image" src="../assets/elyse.png">
+    <img class="ui mini circular image" src="../assets/oswald.png">
     <div class="content">
-      <div class="ui sub header">Elyse</div>
+      <div class="ui sub header">Oswaldo</div>
       Scrum Master - Oswaldo Quiñones
     </div>
   </div>
   <div class="item">
-    <img src="../assets/elyse.png" class="ui mini circular image">
+    <img src="../assets/gerard.png" class="ui mini circular image">
     <div class="content">
-      <div class="ui sub header">Eve</div>
+      <div class="ui sub header">Gerardo</div>
       Project Manager-Gerardo Salcedo Pereira
     </div>
   </div>
