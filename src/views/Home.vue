2aadb551d03f505bba1b891706799168a5ef608c
Gerardo SP <ingenierosalcedo@gmail.com>
2021-10-06 23:06:01 -0500

    Integrante

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <BasicLayouts>
-    <h2>Este es el Home</h2>
+    <h2>Bienvenidos al Home</h2>
     <div class="ui horizontal list">
   <div class="item">
     <img class="ui mini circular image" src="../assets/elyse.png">
     <div class="content">
       <div class="ui sub header">Molly</div>
       Coordinador-Jhon Orozco 
+    </div>
+  </div>
+  <div class="ui horizontal list">
+  <div class="item">
+    <img class="ui mini circular image" src="../assets/elyse.png">
+    <div class="content">
+      <div class="ui sub header">Molly</div>
+      Developer-Yoan 
     </div>
   </div>
   <div class="item">
